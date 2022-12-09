43644f234364fa0450dd90689020d79e86344898
Dennis Falling <dfalling@gmail.com>
2022-12-09 19:29:26 +0800

    Cleanup setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 # vim setup
 mkdir ~/.config
 mkdir ~/.config/nvim
-echo "source ~/.cfg/nvim/index.vim" > ~/.config/nvim/init.vim
+echo "source ~/.cfg/nvim/index.vim" >> ~/.config/nvim/init.vim
 ln -s ~/.cfg/nvim/ftplugin ~/.config/nvim/
 ln -s ~/.cfg/nvim/coc-settings.json ~/.config/nvim/
 
 # other configs
 ln -s ~/.cfg/gitignore ~/.gitignore
 ln -s ~/.cfg/tmux.conf ~/.tmux.conf
-ln -s ~/.cfg/zshrc ~/.zshrc
+echo "source ~/.cfg/zshrc" >> ~/.zshrc
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
