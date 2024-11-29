f437dbadcac18d38ef9a588df90447d2f1ff68df
Dennis Falling <dennis@usebounce.com>
2024-11-29 11:07:22 +0000

    Add space to nerdcomment

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -52,3 +52,8 @@ local prefix = vim.env.XDG_CONFIG_HOME or vim.fn.expand("~/.config")
 vim.opt.undodir = { prefix .. "/nvim/.undo//" }
 vim.opt.backupdir = { prefix .. "/nvim/.backup//" }
 vim.opt.directory = { prefix .. "/nvim/.swp//" }
+
+-- nerdcommenter
+vim.cmd([[
+  let g:NERDSpaceDelims = 1
+]])
